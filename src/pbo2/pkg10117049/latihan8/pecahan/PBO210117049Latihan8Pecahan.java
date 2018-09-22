abd80bfc843b5f463dfae1069e3120b68daa4df3
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-09-22 10:20:09 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latihan8/pecahan/PBO210117049Latihan8Pecahan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latihan8.pecahan;
+
+/**
+ *
+ * @author Gery Akbar Fauzi
+ * Nama                 : Gery Akbar Fauzi
+ * Kelas                : PBO2
+ * Nim                  : 10117049
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan jenis - 
+ *                        jenis tipe data bilangan pecahan 
+ */
+public class PBO210117049Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //deklarasi variabel dengan tipde data bilangan pecahan
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("float : "+ f);
+        System.out.println("double : "+ d);
+    }
+    
+}
